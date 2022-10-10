38db3ded8ea477b8e6490f2b4ebe43a3aa2a3e38
Bruno Andrade <brunoan99@gmail.com>
2022-10-10 03:34:59 -0300

    feat: add Product Interface and Struct

---
+++ application/product.go
@@ -0,0 +1,23 @@
+package application
+
+type IProduct interface {
+	IsValid() (bool, error)
+	Enable() error
+	Disable() error
+	GetID() string
+	GetName() string
+	GetStatus() string
+	GetPrice() float64
+}
+
+const (
+	DISABLED = "disabled"
+	ENABLED  = "enabled"
+)
+
+type Product struct {
+	ID     string
+	Name   string
+	Price  float64
+	Status string
+}
