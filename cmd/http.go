028771970cd947cedb82466bf0021bd30523b4d4
Bruno Andrade <brunoan99@gmail.com>
2022-10-11 04:34:11 -0300

    feat: add Server cli options

--- cmd/http.go
+++ cmd/http.go
@@ -1,12 +1,12 @@
 /*
 Copyright © 2022 NAME HERE <EMAIL ADDRESS>
-
 */
 package cmd
 
 import (
 	"fmt"
 
+	server "github.com/brunoan99/hexagonal-arch/adapters/web"
 	"github.com/spf13/cobra"
 )
 
@@ -21,7 +21,10 @@ Cobra is a CLI library for Go that empowers applications.
 This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("http called")
+		srvr := server.MakeNewWebServer()
+		srvr.Service = productService
+		fmt.Println("WebServer has been started successfully")
+		srvr.Serve()
 	},
 }
 
